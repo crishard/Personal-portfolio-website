e3583cd242dfaed90e3e9221d87d1fa6222134ba
crishard <crislantorres9@gmail.com>
2024-09-28 18:54:44 -0300

    feature: criando card de habilidades

--- src/components/Skills/CardSkill.tsx
+++ src/components/Skills/CardSkill.tsx
@@ -6,7 +6,7 @@ interface ICardSkillProps {
 
 export const CardSkill = ({ img, text, title }: ICardSkillProps) => {
     return (
-        <div className="bg-bg-shade px-4 py-6 drop-shadow-sm hover:shadow-primary rounded-md">
+        <div className="bg-bg-shade px-4 py-6 drop-shadow-sm hover:shadow-primary rounded-md  border-b-4 border-b-transparent hover:border-primary transition-all duration-500">
             <img src={img} alt="skill" className="h-20 w-20" />
             <h3 className="text-xl font-semibold">{title}</h3>
             <p>{text}</p>
