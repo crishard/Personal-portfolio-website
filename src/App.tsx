05dba707975c4fc80300bb009f4ba8b025319948
crishard <crislantorres9@gmail.com>
2024-09-28 18:57:15 -0300

    feature: chamando os componentes no meu app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { AboutMe } from './components/About Me/AboutMe'
+import { Contact } from './components/Contact/Contact'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { HomeSection } from './components/HomeSection/HomeSection'
+import { Portfolio } from './components/Portfolio/Portfolio'
 import { MySkills } from './components/Skills/MySkills'
+import Testimonials from './components/Testimonials/Testimonials'
 import './index.css'
 
 function App() {
@@ -13,6 +17,10 @@ function App() {
       <main className=''>
         <HomeSection />
         <MySkills />
+        <AboutMe />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
       </main>
       <Footer />
     </body>
