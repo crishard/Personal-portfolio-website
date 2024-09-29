f114b52b7cb69c650332f940bbec18a1013eebeb
crishard <crislantorres9@gmail.com>
2024-09-29 12:33:57 -0300

    feature: responsividade na skills section

--- src/components/Skills/MySkills.tsx
+++ src/components/Skills/MySkills.tsx
@@ -6,11 +6,11 @@ import { CardSkill } from "./CardSkill"
 
 export const MySkills = () => {
     return (
-        <article className="px-20 text-body py-20">
+        <article className="px-10 sm:px-20 text-body py-20">
             <p className="font-semibold text-heading-color">My Skills</p>
             <h2 className="text-5xl font-semibold text-heading-color">My Expertise</h2>
 
-            <div className="flex justify-between gap-4 pt-16">
+            <div className="grid grid-cols-4  max-[1010px]:grid-cols-3 justify-between max-[860px]:grid-cols-2 gap-4 pt-16 max-[560px]:grid-cols-1">
                 <CardSkill text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."} img={img1} title={"Strategy & Direction"} />
                 <CardSkill text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."} img={img2} title={"Branding & Logo"} />
                 <CardSkill text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique."} img={img3} title={"UI & UX Design"} />
