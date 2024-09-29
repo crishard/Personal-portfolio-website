6e49c85789156d04bc9f063127e8977ad333406a
crishard <crislantorres9@gmail.com>
2024-09-29 12:34:43 -0300

    feature: responsividade na  section about

--- src/components/About Me/AboutMe.tsx
+++ src/components/About Me/AboutMe.tsx
@@ -1,11 +1,11 @@
 import aboutImage from "../../assets/About Me.png"
 export const AboutMe = () => {
     return (
-        <section className="px-20 py-20 grid grid-cols-2 items-center gap-16 text-body">
-            <img src={aboutImage} alt="me" />
+        <section className="px-10 sm:px-20 py-20 grid grid-cols-2 items-center gap-16 text-body max-[800px]:block ">
+            <img src={aboutImage} alt="me" className="max-[800px]:pb-9"/>
             <div>
                 <p className="font-medium text-heading-color">About</p>
-                <h2 className="text-5xl font-semibold text-heading-color pt-2.5 pb-4">About Me</h2>
+                <h2 className="text-5xl font-semibold text-heading-color pt-2.5 pb-4  max-[960px]:text-4xl  max-[600px]:text-3xl max-[960px]:pb-2">About Me</h2>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu neque, tristique quis duis. Nibh scelerisque ac adipiscing velit non nulla in amet pellentesque.
 
